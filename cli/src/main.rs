ea22d80e04f06538a83c69e3d22c2c6c33f1138b
Alexandru Vasile <alexandru.vasile@parity.io>
2023-11-23 13:53:19 +0200

    cli: Add command to verify bootnodes
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -14,6 +14,7 @@ use std::error::Error;
 enum Command {
     SendExtrinisic(SendExtrinisicOpts),
     DiscoverNetwork(DiscoverNetworkOpts),
+    VerifyBootnodes(BootnodesOpts),
 }
 
 /// Send extrinsic on the p2p network.
@@ -47,6 +48,26 @@ pub struct DiscoverNetworkOpts {
     bootnodes: Vec<String>,
 }
 
+/// Verify bootnodes are reachable on the p2p network.
+///
+/// This will attempt to connect ot each provided bootnode and
+#[derive(Debug, ClapParser)]
+pub struct BootnodesOpts {
+    /// Bootnodes of the chain, must contain a multiaddress together with the peer ID.
+    ///
+    /// For example, "/ip4/127.0.0.1/tcp/30333/ws/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp".
+    #[clap(long, use_value_delimiter = true, value_parser)]
+    bootnodes: Vec<String>,
+    /// Hex-encoded genesis hash of the chain.
+    ///
+    /// When this is provided, the supported p2p protocols of the bootnodes will be
+    /// verified against the provided genesis hash.
+    ///
+    /// For example, "781e4046b4e8b5e83d33dde04b32e7cb5d43344b1f19b574f6d31cbbd99fe738"
+    #[clap(long, short)]
+    genesis: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt().init();
@@ -57,5 +78,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             submit_extrinsics(opts.genesis, opts.bootnodes, opts.extrinsics).await
         }
         Command::DiscoverNetwork(opts) => discover_network(opts.genesis, opts.bootnodes).await,
+        Command::VerifyBootnodes(_opts) => Ok(()),
     }
 }
