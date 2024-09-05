b11bb58f58a1fba27541a3645e40fd21ba78fa31
Alexandru Vasile <alexandru.vasile@parity.io>
2024-09-05 20:14:26 +0300

    bench: Introduce discovery for local network option
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- bench-cli/src/main.rs
+++ bench-cli/src/main.rs
@@ -7,11 +7,13 @@ use serde::Serialize;
 use std::{error::Error, path::PathBuf};
 
 mod discovery_backends;
+mod discovery_local;
 
 /// Command for interacting with the CLI.
 #[derive(Debug, ClapParser)]
 enum Command {
     Discovery(DiscoverBackendsNetworkOpts),
+    DiscoveryLocal(DiscoverLocalBackendsNetworkOpts),
 }
 
 #[derive(Debug, Default, clap::ValueEnum, Clone, Copy, Serialize)]
@@ -52,6 +54,26 @@ pub struct DiscoverBackendsNetworkOpts {
     data_set: Option<PathBuf>,
 }
 
+/// Discover the p2p network.
+#[derive(Debug, ClapParser)]
+pub struct DiscoverLocalBackendsNetworkOpts {
+    /// The number of peers to simulate in the network.
+    #[clap(long)]
+    network_size: usize,
+
+    /// The number of peers discovered after which the discovery process should stop.
+    #[clap(long)]
+    num_peers: usize,
+
+    /// The backend type to use for the discovery process.
+    #[clap(long, short)]
+    backend_type: BackendType,
+
+    /// The data set to use for the discovery process.
+    #[clap(long, value_parser)]
+    data_set: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt()
@@ -61,5 +83,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Command::parse();
     match args {
         Command::Discovery(opts) => discovery_backends::discovery_backends(opts).await,
+        Command::DiscoveryLocal(opts) => {
+            discovery_local::spawn_network(
+                opts,
+                "781e4046b4e8b5e83d33dde04b32e7cb5d43344b1f19b574f6d31cbbd99fe738".to_string(),
+            )
+            .await
+        }
     }
 }
